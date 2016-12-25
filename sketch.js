c6af34c53443e9768a9d9702b3f5abfe5a4f41b4
Allison Berkoy <berkoy@berkoy.com>
2016-12-25 11:12:48 -0500

    more testing

--- sketch.js
+++ sketch.js
@@ -9,8 +9,8 @@ function preload() { //best to load JSON in preload
   //sub key
   var urlny = 'http://api.openweathermap.org/data/2.5/weather?APPID=6fbadd7b22efd6545720c02b303d5deb&q=NewYork,USA&units=imperial'
   var urlchi = 'http://api.openweathermap.org/data/2.5/weather?APPID=6fbadd7b22efd6545720c02b303d5deb&q=Chicago,USA&units=imperial'
-  weatherNY = loadJSON(urlny);
-  weatherChi = loadJSON(urlchi);
+  //weatherNY = loadJSON(urlny);
+  //weatherChi = loadJSON(urlchi);
 }
 
 
@@ -21,8 +21,11 @@ function setup() {
 
   textSize(24);
   textAlign(CENTER);
+  /*
   text("In New York we have " + weatherNY.weather[0].description, width / 2, height / 2 - 60);
   text("and the temperature is " + int(weatherNY.main.temp) + ".", width / 2, height / 2 - 30);
   text("In Chicago they have " + weatherChi.weather[0].description, width / 2, height / 2 + 30);
   text("and the temperature is " + int(weatherChi.main.temp) + ".", width / 2, height / 2 + 60);
+  */
+  text("testing testing", width/2, height/2);
 }
