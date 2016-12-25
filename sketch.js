7f379a5f8780bd860dbb795fcf4a2dabc9e37f1c
Allison Berkoy <berkoy@berkoy.com>
2016-12-25 11:53:16 -0500

    test new api

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
-// example for loading JSON from URL 
-// get API key from http://openweathermap.org/api 
+
 
 
 var weatherNY;
@@ -7,25 +6,31 @@ var weatherChi;
 
 function preload() { //best to load JSON in preload
   //sub key
-  var urlny = 'http://api.openweathermap.org/data/2.5/weather?APPID=6fbadd7b22efd6545720c02b303d5deb&q=NewYork,USA&units=imperial'
-  var urlchi = 'http://api.openweathermap.org/data/2.5/weather?APPID=6fbadd7b22efd6545720c02b303d5deb&q=Chicago,USA&units=imperial'
-  weatherNY = loadJSON(urlny);
+  //var urlny = 'http://api.openweathermap.org/data/2.5/weather?APPID=6fbadd7b22efd6545720c02b303d5deb&q=NewYork,USA&units=imperial'
+  //var urlchi = 'http://api.openweathermap.org/data/2.5/weather?APPID=6fbadd7b22efd6545720c02b303d5deb&q=Chicago,USA&units=imperial'
+ 
+ var urlchi= 'http://api.wunderground.com/api/e9e7ffff3e289a8b/conditions/q/IL/Chicago.json';
+ 
+  //weatherNY = loadJSON(urlny);
   weatherChi = loadJSON(urlchi);
 }
 
 
 function setup() {
   createCanvas(500, 500);
-  console.log(weatherNY);
-  //console.log(weatherChi);
+  //console.log(weatherNY);
+ //console.log(weatherChi);
+ console.log(weatherChi.current_observation.weather);
+  console.log(weatherChi.current_observation.temp_f);
 
   textSize(24);
   textAlign(CENTER);
  
+  /*
   text("In New York we have " + weatherNY.weather[0].description, width / 2, height / 2 - 60);
   text("and the temperature is " + int(weatherNY.main.temp) + ".", width / 2, height / 2 - 30);
   text("In Chicago they have " + weatherChi.weather[0].description, width / 2, height / 2 + 30);
   text("and the temperature is " + int(weatherChi.main.temp) + ".", width / 2, height / 2 + 60);
- 
- // text("testing testing", width/2, height/2);
+ */
+ text("In Chicago it is " + weatherChi.current_observation.weather, width / 2, height / 2 + 30);
 }
